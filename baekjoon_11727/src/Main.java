c72c45173fb1690f60baec6c6bc1d46355b49e6d
baikguk <baikgukhyun@gmail.com>
2023-06-23 21:56:46 +0900

    백준_11727 dp 문제 해결

--- baekjoon_11727/src/Main.java
+++ baekjoon_11727/src/Main.java
@@ -1,35 +1,18 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static Integer[] result_arr = new Integer[1000];
-
-    static public int calc(int n) {
-        if(n==0){
-            result_arr[0] = 0;
-        } else if (n == 1) {
-            result_arr[1] = 1;
-        }else if(n==2){
-            result_arr[2] =3;
-        }else {
-            if(result_arr[n] == null){
-                result_arr[n] = calc(n-1) + calc(n-2);
-            }else{
-
-            }
-
-        }
-        return result_arr[n];
-    }
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
+        sc.close();
+
+        int[] result_arr = new int[n+1];
+        result_arr[0] = 1;
+        result_arr[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            result_arr[i] = (result_arr[i-1]+ 2*result_arr[i-2])%10007;
         }
-        System.out.println(result_arr[n]%10007);
+        System.out.println(result_arr[n]);
     }
-
-
 }
